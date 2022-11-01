3b990dae9bd3abf869ee91d03c066d76a0e3f57d
Golobon <golobon@ya.ru>
2022-11-01 23:03:40 +0300

    Commit

---
+++ src/Lecture11_1/Solution.java
@@ -0,0 +1,33 @@
+package Lecture11_1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+Шифровка
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        try (FileInputStream fIS = new FileInputStream(args[1]);
+             FileOutputStream fOS = new FileOutputStream(args[2])) {
+            switch (args[0]) {
+                case "-e": {
+                    while (fIS.available() > 0) {
+                        fOS.write(fIS.read() + 1);
+                    }
+                }
+                case "-d": {
+                    while (fIS.available() > 0) {
+                        fOS.write(fIS.read() - 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

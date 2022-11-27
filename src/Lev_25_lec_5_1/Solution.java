67f4e021810ae07063aaf36559486b501a6b621c
Golobon <golobon@ya.ru>
2022-11-27 16:54:13 +0300

    Commit

--- src/Lev_25_lec_5_1/Solution.java
+++ src/Lev_25_lec_5_1/Solution.java
@@ -12,19 +12,14 @@ public class Solution {
         BufferedReader bR = new BufferedReader(new InputStreamReader(System.in));
         String path1 = bR.readLine();
         String path2 = bR.readLine();
-        FileReader fR = new FileReader("111.");
-        FileWriter fW = new FileWriter(path2);
-        StringBuilder sB = new StringBuilder();
-        while (fR.ready()) {
-            int x = fR.read();
-            sB.append((char) x);
-        }
-        String[] str = sB.toString().split("[\\d][\\d]");
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].equals("[\\d][\\d]")) {
-            }
-        }
+        BufferedReader bR2 = new BufferedReader(new FileReader(path1));
+        BufferedWriter bW = new BufferedWriter(new FileWriter(path2));
+        while (bR2.ready()) {
+            int x = bR2.read();
+            if (String.valueOf((char)x).matches("[^\\p{Punct}\\r\\n]")) {
+                bW.write((char)x); } }
         bR.close();
-        fR.close();
+        bR2.close();
+        bW.close();
     }
 }

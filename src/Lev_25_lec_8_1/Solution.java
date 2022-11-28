e73a6894856cd360de13d89ebbe74f7708e818d7
Golobon <golobon@ya.ru>
2022-11-28 20:52:08 +0300

    Commit

--- src/Lev_25_lec_8_1/Solution.java
+++ src/Lev_25_lec_8_1/Solution.java
@@ -1,9 +1,6 @@
 package Lev_25_lec_8_1;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.*;
 
 /*
 Ридер обертка
@@ -11,44 +8,29 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static TestString testString = new TestString();
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         PrintStream consoleStream = System.out;
         ByteArrayOutputStream bAOS = new ByteArrayOutputStream();
         PrintStream pS = new PrintStream(bAOS);
         System.setOut(pS);
         testString.printSomething();
         String result = bAOS.toString();
-        Pattern pattern = Pattern.compile("\\b[\\d]+\\b");
-        Matcher matcher = pattern.matcher(result);
-        System.setOut(consoleStream);
-        int[] values = new int[2];
-        int count = 0;
-        int value;
 
-        if (result.contains("+")) {
-            while (matcher.find()) {
-                values[count++] = Integer.parseInt(matcher.group());
-            }
-            value = values[0] + values[1];
-            System.out.println(values[0] + " + " + values[1] + " = " + value);
-        } else if (result.contains("-")) {
-            while (matcher.find()) {
-                values[count++] = Integer.parseInt(matcher.group());
-            }
-            value = values[0] - values[1];
-            System.out.println(values[0] + " - " + values[1] + " = " + value);
-        } else if (result.contains("*")) {
-            while (matcher.find()) {
-                values[count++] = Integer.parseInt(matcher.group());
-            }
-            value = values[0] * values[1];
-            System.out.println(values[0] + " * " + values[1] + " = " + value);
-        }
+        BufferedReader bR = new BufferedReader(new InputStreamReader(System.in));
+        FileOutputStream fOS = new FileOutputStream(bR.readLine());
+        fOS.write(result.getBytes());
+        bR.close();
+        fOS.close();
+
+        System.setOut(consoleStream);
+        System.out.println(result);
     }
+
 
     public static class TestString {
         public void printSomething() {
-            System.out.println("3 + 6 = ");
+            System.out.println("it's a text for testing");
         }
     }
 }

ab033cdabd0b61a0d468cfcabf3f5f0d23cf0337
Golobon <golobon@ya.ru>
2022-12-03 08:42:24 +0300

    Commit

--- src/Lecture_3/Solution.java
+++ src/Lecture_3/Solution.java
@@ -1,62 +1,57 @@
 package Lecture_3;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 
 /*
-Вместе быстрее? Ща проверим :)
+Знакомство с тегами
 */
 
 public class Solution {
-    public static int threadCount = 10;
-    public static int[] testArray = new int[1000];
+    public static int indexFirst;
+    public static int indexNext;
+    public static int count;
+    public static String str = "";
+    public static String teg;
 
-    static {
-        for (int i = 0; i < Solution.testArray.length; i++) {
-            testArray[i] = i;
+    public static int[] Method1(String str) {
+
+        indexFirst = str.indexOf("<" + teg);
+        indexNext = str.indexOf("</" + teg + ">");
+        String strShare = str.substring(indexFirst + teg.length() + 1, indexNext);
+        while (strShare.contains("<" + teg)) {
+            indexFirst = indexNext + 2;
+            indexNext = str.indexOf("</" + teg + ">", indexNext + teg.length() + 3);
+            strShare = str.substring(indexFirst + teg.length() + 1, indexNext);
         }
+        int[] index = new int[2];
+        index[0] = str.indexOf("<" + teg);
+        index[1] = indexNext;
+        str = str.substring(str.indexOf("<" + teg), indexNext + teg.length() + 3);
+        System.out.println(str);
+        return index;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        StringBuffer expectedResult = new StringBuffer();
-        for (int i = 1000 - 1; i >= 0; i--) {
-            expectedResult.append(i).append(" ");
-        }
-
-        initThreads();
-
-        StringBuffer result = new StringBuffer();
-        for (int i = 0; i < testArray.length; i++) {
-            result.append(testArray[i]).append(" ");
-        }
-        System.out.println(result);
-        System.out.println((result.toString()).equals(expectedResult.toString()));
-    }
-
-    public static void initThreads() throws InterruptedException {
-        List<Thread> threads = new ArrayList<Thread>(threadCount);
-        for (int i = 0; i < threadCount; i++) threads.add(new SortThread());
-        for (Thread thread : threads) thread.start();
-        for (Thread thread : threads) thread.join();
-    }
-
-    public static synchronized void sort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] < array[j]) {
-                    int k = array[i];
-                    array[i] = array[j];
-                    array[j] = k;
-                }
+    public static void main(String[] args) throws IOException {
+        teg = "таг";//args[0];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        StringBuilder stringBuilder = new StringBuilder(str);
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(fileName))) {
+            while (reader1.ready()) {
+                stringBuilder.append(reader1.readLine());
             }
         }
-    }
+        str = stringBuilder.toString();
 
-    public static class SortThread extends Thread {
-        @Override
-        public void run() {
-            Solution.sort(testArray);
+        while (str.contains("<" + teg)) {
+            int[] index = Solution.Method1(str);
+            str = str.substring(0, index[0]) + str.substring(index[0] + teg.length() + 2, index[1]) + str.substring(index[1] + teg.length() + 3);
         }
     }
 }
--- src/Lev_25_lec_11_3_1/Solution.java
+++ src/Lev_25_lec_11_3_1/Solution.java
@@ -17,27 +17,42 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        String path;
+        String tagArgument = args[0];
 
+        String path = selectPath();
+
+        String stringWithTags = getString(path);
+
+        List<String> listResult = findTags(tagArgument, stringWithTags);
+
+        for (String x : listResult) {
+            System.out.println(x);
+        }
+    }
+
+    public static String selectPath() throws IOException {
+        String path;
         try (BufferedReader bR = new BufferedReader(new InputStreamReader(System.in))) {
             path = bR.readLine();
         }
+        return path;
+    }
 
+    public static String getString(String path) throws IOException {
         String str;
-
         try (BufferedReader bRF = new BufferedReader(new FileReader(path))) {
             StringBuilder sB = new StringBuilder();
-
             while (bRF.ready()) {
                 sB.append(bRF.readLine());
             }
             str = sB.toString();
         }
+        return str;
+    }
 
-        String v = "таг";//args[0];
-
-        List<String> listStr = new ArrayList<>();
-        List<String> listRes = new ArrayList<>();
+    public static List<String> findTags(String v, String str) {
+        List<String> listTagsStructure = new ArrayList<>();
+        List<String> listWithResult = new ArrayList<>();
 
         String regOp = String.format("<%s", v);
         String regCl = String.format("</%s>", v);
@@ -53,11 +68,11 @@ public class Solution {
             matcherOp.group();
             matcherCl.group();
 
-            listStr.add(matcherOp.start() + "<" + v);
-            listStr.add(matcherCl.start() + "</" + v);
+            listTagsStructure.add(matcherOp.start() + "<" + v);
+            listTagsStructure.add(matcherCl.start() + "</" + v);
         }
 
-        listStr.sort(new Comparator<>() {
+        listTagsStructure.sort(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 int x = Integer.parseInt(o1.replaceAll("\\D", ""));
@@ -66,37 +81,49 @@ public class Solution {
             }
         });
 
-        for (String x : listStr) {
-            System.out.println(x);
-        }
+//        for (String x : listTagsStructure) {
+//            System.out.println(x);
+//        }
 
         int count = 0;
-
         int start = 0;
-
         int end = 0;
+        int indexOp = 0;
+        int indexCl = 0;
+        int flag = 0;
+        int i = 0;
 
-        for (int i = 0; i < listStr.size(); i++) {
-            if (listStr.get(i).contains("<" + v)) {
-                count++;
-                if (end == 0) {
-                    start = Integer.parseInt(listStr.get(i).replaceAll("\\D", ""));
+        while (listTagsStructure.size() > 1) {
+            for (i = 0; i < listTagsStructure.size(); i++) {
+                if (listTagsStructure.get(i).contains("<" + v)) {
+                    count++;
+                    if (flag == 0) {
+                        indexOp = i;
+                        start = Integer.parseInt(listTagsStructure.get(i).replaceAll("\\D", ""));
+                        flag = 1;
+                    }
+                }
+                if (listTagsStructure.get(i).contains("</" + v)) {
+                    count--;
+                    if (count == 0) {
+                        indexCl = i;
+                        end = Integer.parseInt(listTagsStructure.get(i).replaceAll("\\D", "")) + v.length() + 3;
+                    }
+                }
+                if (count == 0 && listTagsStructure.get(i).contains("</" + v)) {
+                    listWithResult.add(str.substring(start, end));
+//                    System.out.println(str.substring(start, end));
+                    start = 0;
+                    end = 0;
+                    flag = 0;
+                    listTagsStructure.remove(indexCl);
+                    listTagsStructure.remove(indexOp);
+                    i = -1;
                 }
             }
-            if (listStr.get(i).contains("</" + v)) {
-                count--;
-                end = Integer.parseInt(listStr.get(i).replaceAll("\\D", "")) + v.length() + 3;
-            }
-            if (count == 0 && listStr.get(i).contains("</" + v)) {
-                listRes.add(str.substring(start, end));
-                start = 0;
-                end = 0;
-            }
-
-            for (String x : listRes) {
-                System.out.println(x);
-            }
         }
+        listTagsStructure = null;
+        return listWithResult;
     }
 }
 
